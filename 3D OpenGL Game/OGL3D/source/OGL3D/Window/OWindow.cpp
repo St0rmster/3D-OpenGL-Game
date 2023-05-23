e261162cd78d4fa7f10d9f0549a68c7ce2be861c
St0rmster <bernardwind6@gmail.com>
2023-05-23 12:34:37 +1200

    Windows

--- 3D OpenGL Game/OGL3D/source/OGL3D/Window/OWindow.cpp
+++ 3D OpenGL Game/OGL3D/source/OGL3D/Window/OWindow.cpp
@@ -2,7 +2,6 @@
 #include <Windows.h>
 #include <assert.h>
 
-
 OWindow::OWindow()
 {
 	WNDCLASSEX wc = {};
@@ -10,10 +9,16 @@ OWindow::OWindow()
 	wc.lpszClassName = L"OGL3DWindow";
 	wc.lpfnWndProc = DefWindowProc;
 
-	RegisterClassEx(&wc));
+	assert(RegisterClassEx(&wc));
 
-	CreateWindowExW(NULL, L"OGL3DWindow",L"St0rmster | OpenGL 3D Game",WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
-		CW_USEDEFAULT, CW_USEDEFAULT, 1024 ,768
+	RECT rc = { 0,0,1024,768 };
+	AdjustWindowRect(&rc, WS_SYSMENU, false);
+
+	m_handle = CreateWindowEx(NULL, L"OGL3DWindow", L"St0rmster | OpenGL 3D Game", WS_SYSMENU, CW_USEDEFAULT, CW_USEDEFAULT,
+	rc.right, rc.bottom, NULL, NULL, NULL, NULL);
+
+	assert(m_handle);
+
 }
 
 OWindow::~OWindow()
